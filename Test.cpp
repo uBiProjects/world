088e19d4dd10ca889cbda08766bdad912400fdab
Sarah Dreher <sdreher@techfak.uni-bielefeld.de>
2014-02-27 15:42:00 +0100

    Test.cpp parser

--- Test.cpp
+++ Test.cpp
@@ -6,19 +6,39 @@ using namespace std;
 int main ()
 {
     string STRING;
+    string temp="";
+    int filerows=0;;
     ifstream infile;
     infile.open ("bla.txt");
-    int a=0;
-    string previousLine="";
-    while(a<1) // To get you all the lines.
-    {
-        getline(infile,STRING); // Saves the line in STRING.
-        if (STRING != previousLine)
-        {
-            previousLine=STRING;
-            cout<<STRING<<endl; // Prints our STRING.
-        }
 
+    string previousLine="";
+    
+    while(!infile.eof()) // Determine number of lines
+    {
+  	getline(infile,STRING);
+  	filerows++;
+    }
+    
+    infile.close();
+    infile.open ("bla.txt");
+    
+    string values[filerows];
+    
+    for(int j=0; !infile.eof();j++)
+    {
+  	getline(infile,STRING);
+  	for(int i=0;i<STRING.length();i++){
+  		if(STRING.at(i)=='\t'){
+  			for(i++;i<STRING.length();i++){
+  				temp+=STRING.at(i);
+  			}
+  		}
+  	}
+  	values[j]=temp;
+  	cout<<temp<<endl;
+  	temp="";
     }
     infile.close();
+    //Wert muss noch returnt werden...
+    //return values;
 }
