d34626c869a0fd6a8075b80f31c77bedcc9c8024
Sarah Dreher <sdreher@techfak.uni-bielefeld.de>
2014-02-27 14:50:24 +0100

    test.cpp etc.

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,24 @@
-int main() {
-	
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string STRING;
+    ifstream infile;
+    infile.open ("bla.txt");
+    int a=0;
+    string previousLine="";
+    while(a<1) // To get you all the lines.
+    {
+        getline(infile,STRING); // Saves the line in STRING.
+        if (STRING != previousLine)
+        {
+            previousLine=STRING;
+            cout<<STRING<<endl; // Prints our STRING.
+        }
+
+    }
+    infile.close();
 }
